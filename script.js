c95c7f96244a84db564effe125a5841ac6928adb
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2024-11-08 19:18:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -335,7 +335,7 @@ const sounds = {
 
 // Playlist sounds
 const playlists = {
-    lofi: new Audio('https://drive.google.com/file/d/1Y9NvTp8tjFxhlxSSgplfmQlQvucUgHAy/view?usp=sharing'),
+    lofi: new Audio('https://github.com/TheRuchirShah/Test-Large-File/blob/main/src/playlist/Bollywood.mp3'),
     jazz: new Audio('https://theruchirshah.github.io/My-Files/files/audio/Train.mp3')
 };
 
